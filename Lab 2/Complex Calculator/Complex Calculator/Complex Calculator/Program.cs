c5569b7a56f16a368699c00e76dd8d47a503e489
Henok Tadesse <henok3t@gmail.com>
2012-09-17 23:01:12 -0700

    lab2

---
+++ Lab 2/Complex Calculator/Complex Calculator/Complex Calculator/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Complex_Calculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ComplexCalculator calc = new ComplexCalculator();
+            calc.RunCalculator();
+        }
+    }
+}
